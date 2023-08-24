6af443f588b16756dd62577ceefcde2356ed4082
coding-to-music <connors.tom@gmail.com>
2023-08-24 16:59:26 -0500

    Build

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -3,19 +3,28 @@
 import { getSession } from "next-auth/react";
 import prisma from "../../../lib/prisma";
 
+// import { useSession } from "next-auth/react";
+
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
 export default async function handle(req, res) {
   const { title, content } = req.body;
 
+  // const { data: session, status } = useSession();
+
+  // const { data: session, status } = useSession();
+
   const session = await getSession({ req });
-  const result = await prisma.post.create({
-    data: {
-      title: title,
-      content: content,
-      author: { connect: { email: session?.user?.email } },
-    },
-  });
-  res.json(result);
+  console.log(session);
+  console.log(req);
+
+  // const result = await prisma.post.create({
+  //   data: {
+  //     title: title,
+  //     content: content,
+  //     author: { connect: { email: session?.user?.email } },
+  //   },
+  // });
+  // res.json(result);
 }
