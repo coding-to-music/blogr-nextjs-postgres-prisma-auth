59dd574ae0bdf44c8b958a31dd268ad24f8b8deb
coding-to-music <connors.tom@gmail.com>
2023-08-25 15:36:52 -0500

    Build

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,33 +1,21 @@
-import getSessionHandler from "../../../pages/api/auth/session";
+// pages/api/post/index.ts
+
+import { getSession } from "next-auth/react";
 import prisma from "../../../lib/prisma";
 
 // POST /api/post
-// Required fields in body: title, email
+// Required fields in body: title
 // Optional fields in body: content
 export default async function handle(req, res) {
-  try {
-    const { title, content } = req.body;
+  const { title, content } = req.body;
 
-    // Check for an active session
-    const session = await getSessionHandler(req);
-
-    if (!session) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-
-    console.log("session", session);
-
-    const result = await prisma.post.create({
-      data: {
-        title: title,
-        content: content,
-        author: { connect: { email: session.user.email } },
-      },
-    });
-
-    res.json(result);
-  } catch (error) {
-    console.error("Error creating post:", error);
-    res.status(500).json({ error: "Error creating post" });
-  }
+  const session = await getSession({ req });
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: session?.user?.email } },
+    },
+  });
+  res.json(result);
 }
