33c34a7dd1eb68b7a37acfbe36fd99704f42d532
coding-to-music <connors.tom@gmail.com>
2023-08-31 14:42:22 -0500

    Build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { Analytics } from "@vercel/analytics/react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./styles.css";
 
 function App({ Component, pageProps }: AppProps) {
   return (
